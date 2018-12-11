145a582e50330b36e8b7db9d8599aeadd5d5e4a6
陳文將 <d36584@gmail.com>
2018-12-11 16:35:39 +0800

    use express

--- index.js
+++ index.js
@@ -1,65 +1,61 @@
-// const http = require("http");
+const express = require('express');
+var url = require('url');
+var fs = require('fs');
+const bodyParser = require('body-parser');
+const app = express();
 
-// const hostname = "127.0.0.1";
-// const PORT = process.env.PORT || 5000;
+const port = process.env.POST || 5000;
 
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader("Content-Type", "text/plain");
-//     res.end("Hello World\n");
-// });
+//使用body-parser中间件 要使用post的话就需要使用body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extends: false }));
 
-// server.listen(PORT);
-// console.log(`Server running at http://${hostname}:${PORT}/`);
+app.get("/", (request, response) => {
+    console.log("Request received...");
+    var pathname = url.parse(request.url).pathname;
+    var ext = pathname.match(/(\.[^.]+|)$/)[0]; //取得后缀名
+    switch (ext) {
+        case ".css":
+        case ".js":
+            fs.readFile("." + request.url, 'utf-8', function(err, data) { //读取内容
+                if (err) throw err;
+                response.writeHead(200, {
+                    "Content-Type": {
+                        ".css": "text/css",
+                        ".js": "application/javascript",
+                    }[ext]
+                });
+                response.write(data);
+                response.end();
+            });
+            break;
+        default:
+            fs.readFile('./index.html', 'utf-8', function(err, data) { //读取内容
+                if (err) throw err;
+                response.writeHead(200, {
+                    "Content-Type": "text/html"
+                });
+                response.write(data);
+                response.end();
+            });
+
+    }
+});
+app.post('/api/getform', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log('email:' + email);
+    console.log('password:' + password);
+
+});
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
 
 
 
 
 
-var server = require("./server");
 
-server.start();
-
-
-
-// var http = require("http");
-// var fs = require('fs');
-// var url = require('url');
-
-// const hostname = "127.0.0.1";
-// const PORT = process.env.PORT || 5000;
-
-// const server = http.createServer((request, response) => {
-//     console.log("Request received...");
-//     var pathname = url.parse(request.url).pathname;
-//     var ext = pathname.match(/(\.[^.]+|)$/)[0]; //取得后缀名
-//     switch (ext) {
-//         case ".css":
-//         case ".js":
-//             fs.readFile("." + request.url, 'utf-8', function(err, data) { //读取内容
-//                 if (err) throw err;
-//                 response.writeHead(200, {
-//                     "Content-Type": {
-//                         ".css": "text/css",
-//                         ".js": "application/javascript",
-//                     }[ext]
-//                 });
-//                 response.write(data);
-//                 response.end();
-//             });
-//             break;
-//         default:
-//             fs.readFile('./index.html', 'utf-8', function(err, data) { //读取内容
-//                 if (err) throw err;
-//                 response.writeHead(200, {
-//                     "Content-Type": "text/html"
-//                 });
-//                 response.write(data);
-//                 response.end();
-//             });
-
-//     }
-// });
-
-// server.listen(PORT);
-// console.log(`Server running at http://${hostname}:${PORT}/`);
+// const server = require("./server");
+// server.start();
