f4493bc260e2cb4d87a74403cbf8cb7ad4284637
陳文將 <d36584@gmail.com>
2018-12-11 16:58:20 +0800

    ss

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 
-const port = process.env.POST || 5000;
+const port = process.env.PORT || 5000;
 
 
 app.use(express.static('public'))
